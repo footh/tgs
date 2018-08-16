efbba3c36fc211804ccdc3c43307620044d22f91
footh <jeff@faath.com>
2018-08-16 16:25:09 -0600

    breaking up the training code

--- train.py
+++ train.py
@@ -10,23 +10,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 CONFIG_DIR = 'config'
 
 
-class TrainingSessionRunHook(tf.train.SessionRunHook):
-
-    def __init__(self, loss_scope=None):
-        self.loss_scope = loss_scope
-
-    def before_run(self, run_context):
-        loss = tf.losses.get_losses(scope=self.loss_scope)[0]
-        global_step = tf.train.get_global_step()
-        return tf.train.SessionRunArgs([loss, global_step])
-
-    def after_run(self, run_context, run_values):
-        loss, global_step = run_values.results
-        if (global_step + 1) % 2000 == 0:
-            raise tf.errors.OutOfRangeError(None, None, "Test out of range")
-
-
 def warm_start(cfg):
+    """
+        Configures a warm start setting for use in warm starting a training session. Allows for a mapping of variables
+        to warm start and a regex of variables to initialize.
+    """
     settings = None
     if cfg.get('checkpoint.warm_start.checkpoint_path') is not None:
         warm_start_map = None
@@ -58,58 +46,74 @@ def move_config(config_file, model_dir):
     return new_config_file
 
 
-def train_and_eval(config_file, model_dir, hooks=None):
+def build_dataset(cfg, epochs=999999):
+    """
+        Builds dataset for use in estimator training
+    """
+    tf.logging.info('Using data class: %s' % cfg.get('data.class'))
+    dataset = data.DataInput.get(cfg.get('data.class'))(cfg.get('data'),
+                                                        batch_size=cfg.get('batch_size'),
+                                                        label_cnt=cfg.get('model.label_cnt'),
+                                                        num_epochs=epochs)
 
-    tf.logging.info('Using Tensorflow version: %s' % tf.__version__)
+    return dataset
 
-    tf.logging.info("Reading config file...")
-    c = config.Config(config_file)
 
-    tf.logging.info('Using data class: %s' % c.get('data.class'))
-    dataset = data.DataInput.get(c.get('data.class'))(c.get('data'),
-                                                      batch_size=c.get('batch_size'),
-                                                      label_cnt=c.get('model.label_cnt'))
+def build_estimator(cfg, model_dir):
+    """
+        Builds estimator to be used in training
+    """
 
-    tf.logging.info('Using model: %s' % c.get('model.class'))
-    model = m.BaseModel.get(c.get('model.class'))(c.get('model'))
+    tf.logging.info('Using model: %s' % cfg.get('model.class'))
+    model = m.BaseModel.get(cfg.get('model.class'))(cfg.get('model'))
 
-    # Using time-based checkpoint saving. There is some weirdness with 'save_checkpoint_secs' and 'throttle_secs' below
-    # that seems to have been resolved here:
-    # https://github.com/tensorflow/tensorflow/commit/3edb609926f2521c726737fc1efeae1572dc6581#diff-bc4a1638bbcd88997adf5e723b8609c7
-    # For now, setting throttle_secs to a minute less than save_checkpoint_secs which seems to work well.
-    # TODO: would like to prevent checkpoint saving AFTER every eval
     run_config = tf.estimator.RunConfig(model_dir=model_dir,
                                         save_checkpoints_steps=None,
-                                        save_checkpoints_secs=c.get('checkpoint.save_seconds'),
-                                        keep_checkpoint_max=c.get('checkpoint.keep'),
-                                        log_step_count_steps=100 if c.get('log_steps') is None else c.get('log_steps'))
+                                        save_checkpoints_secs=cfg.get('checkpoint.save_seconds'),
+                                        keep_checkpoint_max=cfg.get('checkpoint.keep'),
+                                        log_step_count_steps=100 if cfg.get('log_steps') is None else cfg.get('log_steps'))
 
-    params = {'learning_rate': c.get('learning_rate.base'),
-              'learning_rate.exponential_decay': c.get('learning_rate.exponential_decay'),
-              'learning_rate.cosine_decay': c.get('learning_rate.cosine_decay'),
-              'l2_normalize': c.get('l2_normalize'),
-              'l2_weight_decay': c.get('l2_weight_decay'),
-              'adam_epsilon': c.get('optimizer.adam.epsilon'),
-              'ema_decay': c.get('ema_decay'),
-              'clip_grad_norm': c.get('clip_grad_norm'),
-              'map_iou_thresholds': c.get('metric.map_iou.thresholds'),
-              'map_iou_predthresh': c.get('metric.map_iou.pred_thresh')}
+    params = {'learning_rate': cfg.get('learning_rate.base'),
+              'learning_rate.exponential_decay': cfg.get('learning_rate.exponential_decay'),
+              'learning_rate.cosine_decay': cfg.get('learning_rate.cosine_decay'),
+              'l2_normalize': cfg.get('l2_normalize'),
+              'l2_weight_decay': cfg.get('l2_weight_decay'),
+              'adam_epsilon': cfg.get('optimizer.adam.epsilon'),
+              'ema_decay': cfg.get('ema_decay'),
+              'clip_grad_norm': cfg.get('clip_grad_norm'),
+              'map_iou_thresholds': cfg.get('metric.map_iou.thresholds'),
+              'map_iou_predthresh': cfg.get('metric.map_iou.pred_thresh')}
 
     estimator = tf.estimator.Estimator(model_fn=model.model_fn,
                                        config=run_config,
                                        params=params,
-                                       warm_start_from=warm_start(c))
+                                       warm_start_from=warm_start(cfg))
 
-    # tsrh = TrainingSessionRunHook(f"{model.name}/loss")
+    return estimator
+
+
+def train_and_eval(cfg, dataset, estimator, eval_seconds=600, hooks=None):
+    """
+        Performs the estimator's train_and_evaluate method
+    """
+
     train_spec = tf.estimator.TrainSpec(input_fn=lambda: dataset.input_fn(tf.estimator.ModeKeys.TRAIN),
-                                        max_steps=c.get('train_steps'),
+                                        max_steps=cfg.get('train_steps'),
                                         hooks=hooks)
 
-    # Per note above, need to use throttle_secs to affect timed evaluation. Checkpoint setting in are ignored.
+    # Evaluation starts at the minimum of an epoch end or the throttle_secs argument. So this must be coordinated with
+    # the dataset num_epochs
     eval_spec = tf.estimator.EvalSpec(input_fn=lambda: dataset.input_fn(tf.estimator.ModeKeys.EVAL),
-                                      steps=c.get('valid_steps'), throttle_secs=c.get('checkpoint.save_seconds'))
+                                      steps=cfg.get('valid_steps'), throttle_secs=eval_seconds)
 
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    return tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+
+def train(cfg, dataset, estimator, hooks=None):
+    """
+        Performs the estimators train method. Returns the estimator for chaining.
+    """
+    return estimator.train(dataset.input_fn, hook=hooks, steps=cfg.get('train_steps'))
 
 
 def main(_):
@@ -117,15 +121,22 @@ def main(_):
 
     new_config_file = move_config(FLAGS.config_file, FLAGS.model_dir)
 
-    train_and_eval(new_config_file, FLAGS.model_dir)
+    tf.logging.info("Reading config file...")
+    cfg = config.Config(new_config_file)
+
+    dataset = build_dataset(cfg)
+
+    estimator = build_estimator(cfg, FLAGS.model_dir)
+
+    train_and_eval(cfg, dataset, estimator)
 
 
 tf.app.flags.DEFINE_string(
-    'model_dir', './yt8m/training-runs/test',
+    'model_dir', './tgs/training-runs/test',
     'Directory to output the training checkpoints and events')
 
 tf.app.flags.DEFINE_string(
-    'config_file', './yt8m/config/exp-dense.yaml',
+    'config_file', './tgs/config/exp-resnetunet.yaml',
     'File containing the configuration for this training run')
 
 
