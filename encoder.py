b53fc82d507020937ae923c4161cfd28bfd454c1
footh <jeff@faath.com>
2018-08-28 01:06:09 -0600

    reorganize unet models, add resnet v2

--- encoder.py
+++ encoder.py
@@ -1,5 +1,6 @@
 from tensorflow.contrib import slim
 from tensorflow.contrib.slim.python.slim.nets import resnet_v1
+from tensorflow.contrib.slim.python.slim.nets import resnet_v2
 
 
 def resnet_v1_50(inputs,
@@ -45,5 +46,51 @@ def build_resnet50_v1(img_input, l2_weight_decay=0.01, is_training=True, prefix=
     block3 = endpoints[f'{prefix}resnet_v1_50/block3']
     block2 = endpoints[f'{prefix}resnet_v1_50/block2']
     block1 = endpoints[f'{prefix}resnet_v1_50/block1']
+
+    return block1, block2, block3, block4
+
+
+def resnet_v2_50(inputs,
+                 num_classes=None,
+                 is_training=True,
+                 global_pool=True,
+                 output_stride=None,
+                 reuse=None,
+                 scope='resnet_v2_50'):
+    """
+        ResNet-50 model of [1]. See resnet_v2() for arg and return description.
+    """
+    blocks = [
+        resnet_v2.resnet_v2_block('block1', base_depth=64, num_units=3, stride=1),
+        resnet_v2.resnet_v2_block('block2', base_depth=128, num_units=4, stride=2),
+        resnet_v2.resnet_v2_block('block3', base_depth=256, num_units=6, stride=2),
+        resnet_v2.resnet_v2_block('block4', base_depth=512, num_units=3, stride=2),
+    ]
+
+    return resnet_v2.resnet_v2(
+        inputs,
+        blocks,
+        num_classes,
+        is_training=is_training,
+        global_pool=global_pool,
+        output_stride=output_stride,
+        include_root_block=True,
+        reuse=reuse,
+        scope=scope)
+
+
+def build_resnet50_v2(img_input, l2_weight_decay=0.01, is_training=True, prefix=None):
+    """
+        Builds resnet50_v2 model from slim
+
+        Returns the last three block outputs to be used transposed convolution layers
+    """
+
+    with slim.arg_scope(resnet_v2.resnet_arg_scope(weight_decay=l2_weight_decay)):
+        block4, endpoints = resnet_v2_50(img_input, is_training=is_training, global_pool=False)
+
+    block3 = endpoints[f'{prefix}resnet_v2_50/block3']
+    block2 = endpoints[f'{prefix}resnet_v2_50/block2']
+    block1 = endpoints[f'{prefix}resnet_v2_50/block1']
 
     return block1, block2, block3, block4
--- unet_model.py
+++ unet_model.py
@@ -3,17 +3,18 @@ from tgs import encoder
 from tgs import model
 
 
-class ResnetUnet(model.BaseModel):
+class UnetModel(model.BaseModel):
     """
-    Unet with Resnet decoder
+        Unet model that captures common functionality for upsampling
     """
 
     def __init__(self, config_dict, name=None):
         super().__init__(config_dict)
+        assert 'img_size' in config_dict['ext'].keys(), "img_size must be provided"
         assert 'encoder_l2_decay' in config_dict['ext'].keys(), "encoder_l2_decay must be provided"
         assert 'process_channels' in config_dict['ext'].keys(), "process_channels must be provided"
 
-        self.name = 'resnet_unet' if name is None else name
+        self.name = 'unet' if name is None else name
 
     def process_ds_layers(self, ds_layers, regularizer=None):
         """
@@ -22,148 +23,24 @@ class ResnetUnet(model.BaseModel):
         """
         ds_layers_out = []
 
-        # NOTE: default activation for conv2d is tf.nn.relu
-        # NOTE: default uniform xavier_initializer is used for the weights here.
-        index = len(ds_layers) - 1
-        while index >= 0:
-            tf.logging.info(ds_layers[index].shape.as_list())
-            net = tf.layers.conv2d(ds_layers[index], self.config_dict['ext']['process_channels'], 1,
-                                   kernel_regularizer=regularizer, name=f'conv{index+1}')
-            if len(ds_layers_out) > 0:
-                up_layer = ds_layers_out[-1]
-                up_size = [2 * l for l in up_layer.shape.as_list()[1:3]]
-                up = tf.image.resize_images(up_layer, up_size, tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        with tf.variable_scope('process_ds'):
+            # NOTE: default activation for conv2d is tf.nn.relu
+            # NOTE: default uniform xavier_initializer is used for the weights here.
+            index = len(ds_layers) - 1
+            while index >= 0:
+                net = tf.layers.conv2d(ds_layers[index], self.config_dict['ext']['process_channels'], 1,
+                                       kernel_regularizer=regularizer, name=f'conv{index+1}')
+                if len(ds_layers_out) > 0:
+                    up_layer = ds_layers_out[-1]
+                    up_size = tf.shape(up_layer)[1:3] * 2
+                    up = tf.image.resize_images(up_layer, up_size, tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-                net = tf.add(net, up, name=f"fuse{index+1}")
-                net = tf.layers.conv2d(net, self.config_dict['ext']['process_channels'], 3, padding='same',
-                                       kernel_regularizer=regularizer, name=f'fuse_conv{index+1}')
+                    net = tf.add(net, up, name=f"fuse{index+1}")
+                    net = tf.layers.conv2d(net, self.config_dict['ext']['process_channels'], 3, padding='same',
+                                           kernel_regularizer=regularizer, name=f'fuse_conv{index+1}')
 
-            ds_layers_out.append(net)
-            index -= 1
-
-        return ds_layers_out[::-1]
-
-    @staticmethod
-    def residual_ds_layers(ds_layers, regularizer=None):
-        """
-            Perform a residual block on each incoming downsampled layer
-        """
-        residual_output = []
-
-        for i, ds_layer in enumerate(ds_layers):
-            net = tf.layers.conv2d(ds_layer, ds_layer.shape.as_list()[-1], 3, activation=tf.nn.relu, padding='same',
-                                   kernel_regularizer=regularizer, name=f'conv_a{i+1}')
-
-            net = tf.layers.conv2d(net, net.shape.as_list()[-1], 3, activation=tf.nn.relu, padding='same',
-                                   kernel_regularizer=regularizer, name=f'conv_b{i+1}')
-
-            net = tf.nn.relu(tf.add(ds_layer, net, name=f"fuse"))
-            residual_output.append(net)
-
-        return residual_output
-
-    @staticmethod
-    def upsample(ds_layers, img_size, regularizer=None):
-        """
-            Takes in a collection of downsampled layers, applies  transposed convolutions for each input layer returns
-            the results.
-
-            Returns the upsampled layers as an array
-
-            kernel size calculated per here:
-            http://warmspringwinds.github.io/tensorflow/tf-slim/2016/11/22/upsampling-and-image-segmentation-with-tensorflow-and-tf-slim/
-        """
-        upsampled_outputs = []
-
-        for i, ds_layer in enumerate(ds_layers):
-            factor = img_size // ds_layer.shape.as_list()[1]
-            kernel = 2 * factor - factor % 2
-
-            tf.logging.debug(f"layer {i+1} kernel, stride (factor): {kernel, factor}")
-            tf.logging.info(f"Layer shape: {ds_layer.shape.as_list()}")
-
-            # Default uniform xavier_initializer is used for the weights here.
-            net = tf.layers.conv2d_transpose(ds_layer, 1, kernel, factor, padding='same',
-                                             kernel_regularizer=regularizer, name=f'tconv{i+1}')
-
-            upsampled_outputs.append(net)
-
-        return upsampled_outputs
-
-    def build_model(self, inp, mode, regularizer=None):
-
-        net = inp['img']
-        img_size = net.shape.as_list()[1]
-        if img_size != net.shape.as_list()[2]:
-            raise ValueError('Image input must have equal dimensions')
-
-        training = (mode == tf.estimator.ModeKeys.TRAIN)
-
-        with tf.variable_scope('encode'):
-            ds_layers = encoder.build_resnet50_v1(net,
-                                                  l2_weight_decay=self.config_dict['ext']['encoder_l2_decay'],
-                                                  is_training=training,
-                                                  prefix=f'{self.name}/encode/')
-
-        with tf.variable_scope('decode'):
-            with tf.variable_scope('process_ds'):
-                ds_layers = self.process_ds_layers(ds_layers, regularizer=regularizer)
-
-            with tf.variable_scope('residual_ds'):
-                ds_layers = self.residual_ds_layers(ds_layers, regularizer=regularizer)
-
-            with tf.variable_scope('upsample'):
-                us_layers = self.upsample(ds_layers, img_size, regularizer=regularizer)
-
-                logits = tf.add_n(us_layers, name='fuse_us')
-
-                logits = tf.squeeze(logits, axis=-1, name='squeeze')
-
-        return logits
-
-    def logits_to_probs(self, logits):
-        return tf.nn.sigmoid(logits)
-
-    def loss_op(self, labels, logits):
-        return tf.losses.sigmoid_cross_entropy(labels, logits)
-
-
-class ResnetUnet2(model.BaseModel):
-    """
-    Unet with Resnet decoder (same as original but using hardcoded values to handle unknown shapes from 'pad' input)
-    """
-
-    def __init__(self, config_dict, name=None):
-        super().__init__(config_dict)
-        assert 'encoder_l2_decay' in config_dict['ext'].keys(), "encoder_l2_decay must be provided"
-        assert 'process_channels' in config_dict['ext'].keys(), "process_channels must be provided"
-
-        self.name = 'resnet_unet' if name is None else name
-
-    def process_ds_layers(self, ds_layers, regularizer=None):
-        """
-            Process the downsample layers by running a convolution to get to desired output channels and upsampling each
-            deeper layer to fuse with layer right above. Fused layers get a final 3x3 convolution.
-        """
-        ds_layers_out = []
-
-        # NOTE: default activation for conv2d is tf.nn.relu
-        # NOTE: default uniform xavier_initializer is used for the weights here.
-        index = len(ds_layers) - 1
-        while index >= 0:
-            net = tf.layers.conv2d(ds_layers[index], self.config_dict['ext']['process_channels'], 1,
-                                   kernel_regularizer=regularizer, name=f'conv{index+1}')
-            if len(ds_layers_out) > 0:
-                up_layer = ds_layers_out[-1]
-                up_size = tf.shape(up_layer)[1:3] * 2
-                up = tf.image.resize_images(up_layer, up_size, tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-                net = tf.add(net, up, name=f"fuse{index+1}")
-                net = tf.layers.conv2d(net, self.config_dict['ext']['process_channels'], 3, padding='same',
-                                       kernel_regularizer=regularizer, name=f'fuse_conv{index+1}')
-
-            ds_layers_out.append(net)
-            index -= 1
+                ds_layers_out.append(net)
+                index -= 1
 
         return ds_layers_out[::-1]
 
@@ -171,26 +48,24 @@ class ResnetUnet2(model.BaseModel):
         """
             Perform a residual block on each incoming downsampled layer
         """
-        filters = []
-
         residual_output = []
 
-        for i, ds_layer in enumerate(ds_layers):
-            net = tf.layers.conv2d(ds_layer, self.config_dict['ext']['process_channels'], 3,
-                                   activation=tf.nn.relu, padding='same',
-                                   kernel_regularizer=regularizer, name=f'conv_a{i+1}')
+        with tf.variable_scope('residual_ds'):
+            for i, ds_layer in enumerate(ds_layers):
+                net = tf.layers.conv2d(ds_layer, self.config_dict['ext']['process_channels'], 3,
+                                       activation=tf.nn.relu, padding='same',
+                                       kernel_regularizer=regularizer, name=f'conv_a{i+1}')
 
-            net = tf.layers.conv2d(net, self.config_dict['ext']['process_channels'], 3,
-                                   activation=tf.nn.relu, padding='same',
-                                   kernel_regularizer=regularizer, name=f'conv_b{i+1}')
+                net = tf.layers.conv2d(net, self.config_dict['ext']['process_channels'], 3,
+                                       activation=tf.nn.relu, padding='same',
+                                       kernel_regularizer=regularizer, name=f'conv_b{i+1}')
 
-            net = tf.nn.relu(tf.add(ds_layer, net, name=f"fuse"))
-            residual_output.append(net)
+                net = tf.nn.relu(tf.add(ds_layer, net, name=f"fuse"))
+                residual_output.append(net)
 
         return residual_output
 
-    @staticmethod
-    def upsample(ds_layers, img_size, regularizer=None):
+    def upsample(self, ds_layers, regularizer=None):
         """
             Takes in a collection of downsampled layers, applies  transposed convolutions for each input layer returns
             the results.
@@ -202,28 +77,65 @@ class ResnetUnet2(model.BaseModel):
         """
         upsampled_outputs = []
 
-        shapes = [32, 16, 8, 4]
+        img_size = self.config_dict['ext']['img_size']
+        shapes = [img_size // (2 ** i) for i in range(2, 6)]
 
-        for i, ds_layer in enumerate(ds_layers):
-            factor = img_size // shapes[i]
-            kernel = 2 * factor - factor % 2
+        with tf.variable_scope('upsample'):
+            for i, ds_layer in enumerate(ds_layers):
+                factor = img_size // shapes[i]
+                kernel = 2 * factor - factor % 2
 
-            # tf.logging.debug(f"layer {i+1} kernel, stride (factor): {kernel, factor}")
-            # tf.logging.info(f"Layer shape: {ds_layer.shape.as_list()}")
+                # tf.logging.debug(f"layer {i+1} kernel, stride (factor): {kernel, factor}")
+                # tf.logging.info(f"Layer shape: {ds_layer.shape.as_list()}")
 
-            # Default uniform xavier_initializer is used for the weights here.
-            net = tf.layers.conv2d_transpose(ds_layer, 1, kernel, factor, padding='same',
-                                             kernel_regularizer=regularizer, name=f'tconv{i+1}')
+                # Default uniform xavier_initializer is used for the weights here.
+                net = tf.layers.conv2d_transpose(ds_layer, 1, kernel, factor, padding='same',
+                                                 kernel_regularizer=regularizer, name=f'tconv{i+1}')
 
-            upsampled_outputs.append(net)
+                upsampled_outputs.append(net)
 
         return upsampled_outputs
+
+    def decoder(self, ds_layers, regularizer=None):
+        with tf.variable_scope('decode'):
+            ds_layers = self.process_ds_layers(ds_layers, regularizer=regularizer)
+
+            ds_layers = self.residual_ds_layers(ds_layers, regularizer=regularizer)
+
+            us_layers = self.upsample(ds_layers, regularizer=regularizer)
+
+            with tf.variable_scope('upsample'):
+                logits = tf.add_n(us_layers, name='fuse_us')
+
+                logits = tf.squeeze(logits, axis=-1, name='squeeze')
+
+        return logits
+
+    def build_model(self, inp, mode, regularizer=None):
+        raise NotImplementedError()
+
+    def logits_to_probs(self, logits):
+        return tf.nn.sigmoid(logits)
+
+    def loss_op(self, labels, logits):
+        return tf.losses.sigmoid_cross_entropy(labels, logits)
+
+
+class ResnetV1Unet(UnetModel):
+    """
+        Unet with Resnet 50 v1 decoder
+    """
+
+    def __init__(self, config_dict, name=None):
+        super().__init__(config_dict)
+
+        self.name = 'resnet_unet' if name is None else name
 
     def build_model(self, inp, mode, regularizer=None):
 
         net = inp['img']
 
-        img_size = 128
+        img_size = self.config_dict['ext']['img_size']
 
         training = (mode == tf.estimator.ModeKeys.TRAIN)
 
@@ -233,24 +145,33 @@ class ResnetUnet2(model.BaseModel):
                                                   is_training=training,
                                                   prefix=f'{self.name}/encode/')
 
-        with tf.variable_scope('decode'):
-            with tf.variable_scope('process_ds'):
-                ds_layers = self.process_ds_layers(ds_layers, regularizer=regularizer)
-
-            with tf.variable_scope('residual_ds'):
-                ds_layers = self.residual_ds_layers(ds_layers, regularizer=regularizer)
-
-            with tf.variable_scope('upsample'):
-                us_layers = self.upsample(ds_layers, img_size, regularizer=regularizer)
-
-                logits = tf.add_n(us_layers, name='fuse_us')
-
-                logits = tf.squeeze(logits, axis=-1, name='squeeze')
+        logits = self.decoder(ds_layers, regularizer=regularizer)
 
         return logits
 
-    def logits_to_probs(self, logits):
-        return tf.nn.sigmoid(logits)
 
-    def loss_op(self, labels, logits):
-        return tf.losses.sigmoid_cross_entropy(labels, logits)
+class ResnetV2Unet(UnetModel):
+    """
+        Unet with Resnet 50 v2 decoder
+    """
+
+    def __init__(self, config_dict, name=None):
+        super().__init__(config_dict)
+
+        self.name = 'resnetv2_unet' if name is None else name
+
+    def build_model(self, inp, mode, regularizer=None):
+
+        net = inp['img']
+
+        training = (mode == tf.estimator.ModeKeys.TRAIN)
+
+        with tf.variable_scope('encode'):
+            ds_layers = encoder.build_resnet50_v2(net,
+                                                  l2_weight_decay=self.config_dict['ext']['encoder_l2_decay'],
+                                                  is_training=training,
+                                                  prefix=f'{self.name}/encode/')
+
+        logits = self.decoder(ds_layers, regularizer=regularizer)
+
+        return logits
